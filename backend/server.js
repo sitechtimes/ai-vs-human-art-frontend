4d1ddd0a1f79eb73b96c8c34ff7257cf71beae12
Richard M. <richardm246@nycstudents.net>
2024-09-13 10:32:31 -0400

    NPM i

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello MEVN Stack!");
+  res.send("Hello MEVN s Stack!");
+});
+// Serve Vue app
+app.use(express.static(path.join(__dirname, "vue-app/dist")));
+
+// Handle all other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "vue-app/dist/index.html"));
 });
 
 app.listen(port, () => {
