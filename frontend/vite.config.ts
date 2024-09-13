09fa7e76dd809393c297a59f6d79716769ff99d3
Richard M. <richardm246@nycstudents.net>
2024-09-13 11:10:46 -0400

    MEVN Running on normal build. check scripts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
