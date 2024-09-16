5a37e07378920863296b855e4861355fbff1a12f
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2024-09-16 10:59:27 -0400

    god i really want ot kms!!!!!!!
    imported menubar component from primevue

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <Menubar :model="items">
+      <template #start>
+        <!-- logo should be here -->
+      </template>
+      <template #item="{ item, props, hasSubmenu, root }">
+        <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+          <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+            <span :class="item.icon" />
+            <span class="ml-2">{{ item.label }}</span>
+          </a>
+        </router-link>
+        <!-- <a v-ripple class="flex items-center" v-bind="props.action">
+          <span :class="item.icon" />
+          <span class="ml-2">{{ item.label }}</span>
+          <Badge
+            v-if="item.badge"
+            :class="{ 'ml-auto': !root, 'ml-2': root }"
+            :value="item.badge"
+          />
+          <span
+            v-if="item.shortcut"
+            class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1"
+            >{{ item.shortcut }}</span
+          >
+          <i
+            v-if="hasSubmenu"
+            :class="[
+              'pi pi-angle-down',
+              { 'pi-angle-down ml-2': root, 'pi-angle-right ml-auto': !root }
+            ]"
+          ></i>
+        </a> -->
+      </template>
+      <template #end>
+        <div>
+          <!-- user profile -->
+        </div>
+      </template>
+    </Menubar>
+  </div>
+</template>
+
+<script setup>
+import Menubar from 'primevue/menubar'
+
+const items = [
+  {
+    route: '/',
+    icon: 'n/a',
+    label: 'Home',
+    badge: false,
+    shortcute: false,
+    hasSubmeny: false
+  }
+]
+</script>
+
+<style scoped></style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+    <Header></Header>
+  </div>
+</template>
+
+<script setup>
+import Header from '../components/Header.vue'
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
