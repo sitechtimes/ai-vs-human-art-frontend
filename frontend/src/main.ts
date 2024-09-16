5de244caeef1756b32df7c825c7d63cd83cbf2f8
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2024-09-16 10:41:24 -0400

    set up primevue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,10 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  unstyled: true
+})
 
 app.mount('#app')
