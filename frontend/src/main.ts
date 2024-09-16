1e5ba60deb86203134ad6b0437c89d5251e4b361
TinaGu0 <144460946+TinaGu0@users.noreply.github.com>
2024-09-16 10:57:24 -0400

    base, tab bar, submit, combo

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import './assets/themes.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,7 +7,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 
 app.use(createPinia())
 app.use(router)
---
+++ frontend/src/views/GamePage.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="themes">
+        <Tabs>
+            <TabList>
+                <Tab value="0">Randomized</Tab>
+                <Tab value="1">Realistic</Tab>
+                <Tab value="2">Anime</Tab>
+                <Tab value="3">Photography</Tab>
+                <Tab value="4">Still Life</Tab>
+            </TabList>
+        </Tabs>
+    </div>
+    <div id="options">
+
+    </div>
+    <div id="submit">
+        <Button label="Submit" @click="visible = true"></Button>
+        <p>COMBO: {{ combo }}</p>
+    </div>
+    <div id="result">
+        <Dialog v-model:visible="visible">
+            <p>your answer is correct / incorrect or whatever</p>
+            <Button label="Try Again?" @click="visible = false"></Button>
+        </Dialog>
+    </div>
+</template>
+
+<script setup lang="ts">
+//@ts-ignore
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
+import Tabs from 'primevue/tabs';
+import TabList from 'primevue/tablist';
+import Tab from 'primevue/tab';
+import { ref } from 'vue'
+
+const visible = ref(false)
+const combo = ref(0)
+
+</script>
+
+<style scoped>
+
+</style>
