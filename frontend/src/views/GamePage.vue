814f2eb9fb12117171deaa3e4d2b8063393e8d0a
TinaGu0 <144460946+TinaGu0@users.noreply.github.com>
2024-09-17 10:58:52 -0400

    images

--- frontend/src/views/GamePage.vue
+++ frontend/src/views/GamePage.vue
@@ -2,19 +2,22 @@
     <div id="themes">
         <Tabs>
             <TabList>
-                <Tab value="0">Randomized</Tab>
-                <Tab value="1">Realistic</Tab>
-                <Tab value="2">Anime</Tab>
-                <Tab value="3">Photography</Tab>
-                <Tab value="4">Still Life</Tab>
+                <Tab v-for="tab in tabs" :value="tab.value">{{ tab.name }}</Tab>
             </TabList>
+            <TabPanels>
+                <TabPanel v-for="tab in tabs" :value="tab.value">
+                    <p>{{ tab.name }}</p>
+                </TabPanel>
+            </TabPanels>
         </Tabs>
     </div>
     <div id="options">
-
+        <Image :src="img" preview />
+        <p>or</p>
+        <Image :src="img" preview />
     </div>
-    <div id="submit">
-        <Button label="Submit" @click="visible = true"></Button>
+    <Button label="Submit" @click="visible = true"></Button>
+    <div id="footer">
         <p>COMBO: {{ combo }}</p>
     </div>
     <div id="result">
@@ -26,19 +29,28 @@
 </template>
 
 <script setup lang="ts">
-//@ts-ignore
 import Button from 'primevue/button';
 import Dialog from 'primevue/dialog';
 import Tabs from 'primevue/tabs';
 import TabList from 'primevue/tablist';
 import Tab from 'primevue/tab';
+import TabPanels from 'primevue/tabpanels';
+import TabPanel from 'primevue/tabpanel';
+import Image from 'primevue/image';
 import { ref } from 'vue'
+
+const tabs = [{ name: "Randomized", value: "0" }, { name: "Realistic", value: "1" }, { name: "Anime", value: "2" }, { name: "Photogtaphy", value: "3" },  { name: "Still Life", value: "4" }, ]
 
 const visible = ref(false)
 const combo = ref(0)
+const img = "https://th.bing.com/th/id/OIP.R7mawz2gXi7lPv7YigIZhAHaIl?w=186&h=216&c=7&r=0&o=5&pid=1.7"
 
 </script>
 
 <style scoped>
-
+#options {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
 </style>
