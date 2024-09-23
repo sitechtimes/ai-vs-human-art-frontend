dd38c1bdca2f1041165e92e4d11a752d2c377bdb
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2024-09-23 10:59:23 -0400

    deleted the memes sorry

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,20 +1,5 @@
 <template>
-  <div>
-    <h1>the best home page.</h1>
-    <p>
-      the most incredible home page anyone has ever seen. i had a little boy come up to me one
-      evening. he told me "wow, you made the best home page i've ever seen!" and you know what i
-      told him? because i don't know what i told him. i activated the trapdoor beneath his feet and
-      sent him to the dungeon
-    </p>
-    <div style="display: flex; align-items: start">
-      <img src="/fatfatpankocat-panko.gif" />
-      <div style="display: flex; align-items: normal; transform: translate(0, 25px)">
-        <span style="font-size: 2rem">←</span>
-        <img src="/true.jpg" />
-      </div>
-    </div>
-  </div>
+  <div></div>
 </template>
 
 <script setup lang="ts"></script>
