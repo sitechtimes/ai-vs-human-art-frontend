2240e954b5694aadf8c070e4508c858dc16c75a7
katherine <116115224+kaerulean@users.noreply.github.com>
2024-09-24 20:46:46 -0400

    tested out menu bar
    and it owkrs i just have to move it away from app veu now yay

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,20 +2,27 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import PrimeVue from 'primevue/config';
+
+import primeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
+import Ripple from 'primevue/ripple';
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.directive('ripple', Ripple);
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue, {
+app.use(primeVue, {
     theme: {
         preset: Aura
-    }
+    },
+    ripple: true 
 });
 
 app.mount('#app')
+
+
+
