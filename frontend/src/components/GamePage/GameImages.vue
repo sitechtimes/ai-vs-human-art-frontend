113c3fdeaf04e2b75f1ce335736a25903a842a4f
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2024-10-09 10:29:34 -0400

    bsghdasfjkf
    tested working w cloudianry

---
+++ frontend/src/components/GamePage/GameImages.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-row">
+    <AdvancedImage :cldImg="Img1" class="flex m-7 h-[30vw]" />
+    <AdvancedImage :cldImg="Img2" class="flex m-7 h-[30vw]" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Cloudinary } from '@cloudinary/url-gen'
+import { AdvancedImage } from '@cloudinary/vue'
+import { fill } from '@cloudinary/url-gen/actions/resize'
+
+// Create a Cloudinary instance and set your cloud name.
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: 'dbrjbu4kt'
+  }
+})
+
+// Instantiate a CloudinaryImage object for the image with the public ID, 'docs/models'.
+const Img1 = cld.image('cld-sample-5')
+const Img2 = cld.image('cld-sample-4')
+
+// Resize to 250 x 250 pixels using the 'fill' crop mode.
+Img1.resize(fill().width(250).height(250))
+Img2.resize(fill().width(250).height(250))
+</script>
+
+<style scoped></style>
---
+++ frontend/src/views/TestView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <GameImages />
+  </div>
+</template>
+
+<script setup lang="ts">
+import GameImages from '../components/GamePage/GameImages.vue'
+</script>
+
+<style lang="scss" scoped></style>
