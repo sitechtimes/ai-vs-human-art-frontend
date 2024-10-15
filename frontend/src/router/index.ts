5e0b88907c8d740090838ce09e97a70e8a7e4cad
TinaGu0 <144460946+TinaGu0@users.noreply.github.com>
2024-10-15 10:54:55 -0400

    :3

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -26,7 +26,12 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ErrorView.vue')
-    }
+    },
+    {
+      path: '/profile',
+      name: 'profilepage',
+      component: () => import('../views/ProfileView.vue')
+    },
   ]
 })
 
---
+++ frontend/src/views/ProfileView.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>Profile Page</p>
+    <div class="border-2 ">
+        <img src="/nagi.jpg" alt="placeholder avatar" class="rounded-full h-36 m-3 ..."/>
+    </div>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import InputText from 'primevue/inputtext';
+
+</script>
+
+<style scoped>
+
+</style>
