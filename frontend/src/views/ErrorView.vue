3f6c925505d47c24bda0348662dd519964ee9d61
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2024-10-23 21:40:12 -0400

    changes the link to a button for color and sized panko cat down

--- frontend/src/views/ErrorView.vue
+++ frontend/src/views/ErrorView.vue
@@ -2,21 +2,23 @@
   <div class="w-full flex flex-col gap-2 items-center pt-5">
     <h1 class="font-black text-6xl">404 Page</h1>
     <div class="flex text-2xl pt-2 gap-2">
-      <p>Sorry you've attempted to reach a page that doesn't exist</p>
+      <p>Sorry! You've attempted to reach a page that doesn't exist</p>
       <span class="cursor-pointer whitespace-nowrap flex flex-nowrap" @click="sad = false">
         :
         <span class="block transition-transform" :class="{ 'rotate-180': !sad }">C</span>
       </span>
     </div>
     <p class="text-2xl">Please try again or try a different page.</p>
-    <RouterLink class="underline text-xl" to="/">Go home</RouterLink>
-    <img src="/fatfatpankocat-panko.gif" v-if="!sad" />
+    <div class="pt-2">
+      <Button as="router-link" label="Go Home" to="/" />
+    </div>
+    <img class="size-2/5" src="/fatfatpankocat-panko.gif" v-if="!sad" />
   </div>
 </template>
 
 <script setup lang="ts">
+import Button from 'primevue/button'
 import { ref } from 'vue'
-import { RouterLink } from 'vue-router'
 
 const sad = ref(true)
 </script>
