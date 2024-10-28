0688c951dab1e88339d402ae96fa26339ef56663
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2024-10-28 10:31:23 -0400

    afdbv gsdfkm hndbdj mentally sending leo death threats may he suffer

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="grid grid-flow-row auto-rows-max">
-    <PersonComponent />
-    <PersonComponent />
+  <div class="grid grid-cols-2 grid-rows-2 justify-items-center m-7">
+    <PersonComponent v-for="people in persons" :key="people" :person="people" />
   </div>
 </template>
 
 <script setup>
 import PersonComponent from '../components/PersonComponent.vue'
+
+const persons = [
+  {
+    name: 'Katherine Zeng'
+  },
+  {
+    name: 'Tina Guo'
+  },
+  {
+    name: 'Richard Masak'
+  },
+  {
+    name: 'Yifan Zhang'
+  }
+]
 </script>
 
 <style scoped></style>
