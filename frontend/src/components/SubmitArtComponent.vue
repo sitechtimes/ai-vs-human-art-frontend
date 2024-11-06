ff8d761e047e5123f9ab7df3f4652d5f38eaae33
TinaGu0 <144460946+TinaGu0@users.noreply.github.com>
2024-11-06 11:01:50 -0500

    idk what im doing (lawrence help why is it returning 422 error?? the type should (?) be correct)

--- frontend/src/components/SubmitArtComponent.vue
+++ frontend/src/components/SubmitArtComponent.vue
@@ -2,22 +2,21 @@
   <div>
     <Fieldset legend="Submit your own art!">
       <h2>TOS</h2>
-      <ScrollPanel class="min-h-80">
-        <p></p>
+      <ScrollPanel class="min-h-64">
+        <p>{{ idol }}</p>
       </ScrollPanel>
       <p>Check the box below to accept the TOS</p>
       <Checkbox v-model="checked" :binary="true" />
-      <div v-if="checked">
+      <div>
         <FileUpload
           ref="fileupload"
           mode="basic"
-          name="demo[]"
           url="/api/upload"
           accept="image/*"
           :maxFileSize="1000000"
-          @upload="onUpload"
+          @upload="uploadFile"
         />
-        <Button @click="uploadFile" aria-label="Upload Button">Upload</Button>
+        <Button v-if="checked" @click="uploadFile" aria-label="Upload Button">Upload</Button>
       </div>
     </Fieldset>
   </div>
@@ -30,18 +29,28 @@ import FileUpload from 'primevue/fileupload'
 import Checkbox from 'primevue/checkbox'
 import ScrollPanel from 'primevue/scrollpanel'
 import Button from 'primevue/button'
+import { useImageStore } from '../stores/images'
 
 const checked = ref(false)
 const file = ref()
+const imageStore = useImageStore()
 
-const uploadFile = () => {
-  file.value.upload()
-  console.log(file.value)
+const uploadFile = async () => {
+  const type = ref("human")
+  try {
+    const res = await imageStore.uploadImage(type.value, file.value)
+    if (res !== null) {
+      console.log('yippee', res)
+    }
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-const onUpload = () => {
-  console.log('uploaded')
-}
+//obviously the most important part of the code...
+const idol =
+  'Muteki no egao de arasu media Shiritai sono himitsu misuteriasu Nuketeru toko sae kanojo no eria Kanpeki de usotsuki na kimi wa Tensaitekina aidoru sama Kyou nani tabeta? Suki na hon wa? Asobi ni iku nara doko ni iku no? Nanimo tabetenai, sore wa naisho Nani wo kikaretemo norari kurari Sou tantan to, dakedo sansan to Miesou de mienai himitsu wa mitsu no aji Are mo nai, nai, nai Kore mo nai, nai, nai Suki na taipu wa? Aite wa? Saa kotaete'
+
 </script>
 
 <style scoped></style>
---
+++ frontend/src/stores/images.ts
@@ -0,0 +1,26 @@
+import { defineStore } from 'pinia'
+
+export const useImageStore = defineStore('image', () => {
+    
+    const uploadImage = async (type: string, link: File,) => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                type: type,
+                link: link
+            })
+          }
+          try {
+            const res = await fetch('http://localhost:3000/api/auth/register', requestOptions)
+            if (!res.ok) {
+              throw new Error(`HTTP error! status: ${res.status}`)
+            }
+          } catch (error) {
+            console.error(error)
+          }
+    }
+    return {
+        uploadImage
+    }
+})
