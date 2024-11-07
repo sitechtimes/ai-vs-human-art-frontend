2cce6f965f52a145ce11cebbb90641c10f619810
TinaGu0 <144460946+TinaGu0@users.noreply.github.com>
2024-11-07 10:58:51 -0500

    literally the most authorized user ever

--- frontend/src/stores/images.ts
+++ frontend/src/stores/images.ts
@@ -1,26 +1,25 @@
 import { defineStore } from 'pinia'
 
 export const useImageStore = defineStore('image', () => {
-    
-    const uploadImage = async (type: string, link: File,) => {
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                type: type,
-                link: link
-            })
-          }
-          try {
-            const res = await fetch('http://localhost:3000/api/auth/register', requestOptions)
-            if (!res.ok) {
-              throw new Error(`HTTP error! status: ${res.status}`)
-            }
-          } catch (error) {
-            console.error(error)
-          }
+  const uploadImage = async (type: string, link: File) => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        type: type,
+        link: link
+      })
     }
-    return {
-        uploadImage
+    try {
+      const res = await fetch('http://localhost:3000/items/upload', requestOptions)
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`)
+      }
+    } catch (error) {
+      console.error(error)
     }
+  }
+  return {
+    uploadImage
+  }
 })
