5194ac3192cd568469320f5e1e8f6c4978f63a81
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2024-11-11 21:49:17 -0500

    bleh

--- frontend/src/stores/image.ts
+++ frontend/src/stores/image.ts
@@ -5,4 +5,39 @@ export const useArt = defineStore('image', () => {
   //state
   const aiArt = ref(null)
   const humanArt = ref(null)
+
+  //actions
+  const getRandomHuman = async () => {
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    try {
+      const res = await fetch('http://localhost:3000/routes/random?type=human')
+      if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`)
+      const data = res.json()
+      return data
+    } catch (error) {
+      console.error('Human Art Error', error)
+    }
+  }
+
+  const getRandomAI = async () => {
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    try {
+      const res = await fetch('http://localhost:3000/routes/random?type=ai')
+      if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`)
+      const data = await res.json()
+      aiArt.value = data
+    } catch (error) {
+      console.error('Human Art Error', error)
+    }
+  }
 })
