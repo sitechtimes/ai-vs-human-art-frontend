36de5740d4680a2099356fcd646e710618308ee2
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-11-16 01:28:06 -0500

    i'll turn into a fossil

--- frontend/src/components/GamePage/GameArt.vue
+++ frontend/src/components/GamePage/GameArt.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="flex flex-row">
-    <Button label="Submit" @click="getArt" />
-    <img :src="firstImage.value" alt="" class="flex m-7 h-[30vw]" />
-    <img :src="secondImage.value" alt="" class="flex m-7 h-[30vw]" />
+  <div class="flex flex-col gap-2">
+    <Button label="Fetch" @click="getArt" :disabled="loading" />
+    <div v-if="!loading && artPieces.length" class="flex flex-col items-center max-w-screen">
+      <h1 class="text-8xl font-extrabold italic font-serif">WHO WOULD WIN</h1>
+      <div class="flex flex-row gap-2 w-full justify-between items-center">
+        <img :src="artPieces[0]" alt="" class="w-5/12" />
+        <span>vs</span>
+        <img :src="artPieces[1]" alt="" class="w-5/12" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,52 +16,21 @@
 import Button from 'primevue/button'
 
 import { ref } from 'vue'
-import { useArt } from '../../stores/image.ts'
+import { useArtStore } from '../../stores/art.ts'
 
-const artStore = useArt()
-const artPieces = []
-const firstImage = ref('')
-const secondImage = ref('')
-const art = []
-
-const getAI = async () => {
-  try {
-    await artStore.getRandomAI()
-    artPieces.push(artStore.aiArt)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-const getHuman = async () => {
-  try {
-    await artStore.getRandomHuman()
-    console.log(artStore.humanArt)
-    artPieces.push(artStore.humanArt)
-  } catch (error) {
-    console.log(error)
-  }
-}
+const artStore = useArtStore()
+const artPieces = ref([])
+const loading = ref(false)
 
 const getArt = async () => {
-  await getAI()
-  await getHuman()
-  // art = JSON.parse(JSON.stringify(artPieces))
-  console.log(artPieces[1])
-  const random = Math.round(Math.random())
-  console.log(artPieces)
-  console.log(random)
-  if (random) {
-    console.log(artPieces[1])
-    artPieces[1] = firstImage.value
-    artPieces[0] = secondImage.value
-    console.log(artPieces[1])
-    console.log(firstImage.value, secondImage.value)
-  } else {
-    artPieces[0] = firstImage.value
-    artPieces[1] = secondImage.value
-    console.log(firstImage.value, secondImage.value)
-  }
+  loading.value = true
+  artPieces.value = [await artStore.getRandomArt('human'), await artStore.getRandomArt('ai')]
+  if (artPieces.value.some((el) => el === null)) {
+    alert('failed to fetch (womp womp)')
+    artPieces.value = []
+  } else if (Math.random() < 0.5) artPieces.value.reverse()
+
+  loading.value = false
 }
 </script>
 
