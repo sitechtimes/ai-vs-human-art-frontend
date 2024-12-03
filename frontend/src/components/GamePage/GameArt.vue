10024d30d5357a4f76895f6137cec8d81fef7d80
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2024-12-03 18:51:49 -0500

    moved

--- frontend/src/components/GamePage/GameArt.vue
+++ frontend/src/components/GamePage/GameArt.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col gap-2">
-    <Button label="Fetch" @click="getArt" :disabled="loading" />
+    <!-- <Button label="Fetch" @click="getArt" :disabled="loading" /> -->
     <div v-if="!loading && artPieces.length" class="flex flex-col items-center max-w-screen">
-      <h1 class="text-8xl font-extrabold italic font-serif">WHO WOULD WIN</h1>
+      <!-- <h1 class="text-8xl font-extrabold italic font-serif">WHO WOULD WIN</h1> -->
       <div class="flex flex-row gap-2 w-full justify-between items-center">
         <img :src="artPieces[0]" alt="" class="w-5/12" />
         <span>vs</span>
@@ -15,15 +15,15 @@
 <script setup>
 import Button from 'primevue/button'
 
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useArtStore } from '../../stores/art.ts'
 
 const artStore = useArtStore()
 const artPieces = ref([])
-const loading = ref(false)
+// const loading = ref(false)
 
 const getArt = async () => {
-  loading.value = true
+  // loading.value = true
   artPieces.value = [await artStore.getRandomArt('human'), await artStore.getRandomArt('ai')]
   if (artPieces.value.some((el) => el === null)) {
     alert('Failed to fetch art, please ')
@@ -31,8 +31,12 @@ const getArt = async () => {
   } else if (Math.random() < 0.5) {
     artPieces.value.reverse()
   }
-  loading.value = false
+  // loading.value = false
 }
+
+onMounted(() => {
+  getArt()
+})
 </script>
 
 <style scoped></style>
