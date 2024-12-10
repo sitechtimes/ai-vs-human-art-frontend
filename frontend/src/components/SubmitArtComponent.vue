672fb81257c232ac171e21eac14226ab17b00a00
TinaGu0 <144460946+TinaGu0@users.noreply.github.com>
2024-12-10 11:00:50 -0500

    hi

--- frontend/src/components/SubmitArtComponent.vue
+++ frontend/src/components/SubmitArtComponent.vue
@@ -52,7 +52,7 @@ import { useUserStore } from '@/stores/user'
 
 const imageStore = useImageStore()
 const checked = ref(false)
-const type = ref('human')
+const type = ref('unscreened')
 const link = ref('')
 const message = ref('')
 const file = ref<File>()
@@ -93,7 +93,6 @@ async function submit() {
   }
   uploading.value = false
 }
-
 //obviously the most important part of the code...
 const idol =
   'Muteki no egao de arasu media Shiritai sono himitsu misuteriasu Nuketeru toko sae kanojo no eria Kanpeki de usotsuki na kimi wa Tensaitekina aidoru sama Kyou nani tabeta? Suki na hon wa? Asobi ni iku nara doko ni iku no? Nanimo tabetenai, sore wa naisho Nani wo kikaretemo norari kurari Sou tantan to, dakedo sansan to Miesou de mienai himitsu wa mitsu no aji Are mo nai, nai, nai Kore mo nai, nai, nai Suki na taipu wa? Aite wa? Saa kotaete'
