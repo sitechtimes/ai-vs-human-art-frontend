b42ae5a3fe2d1c7b8e3481bf49eb9c32c9c60dab
TinaGu0 <144460946+TinaGu0@users.noreply.github.com>
2024-12-13 11:02:11 -0500

    done...?

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,20 +1,18 @@
 <template>
-  <div v-if="user">
-    <div class="flex-col space-y-2.5 mt-6 w-96 place-items-center place-self-center">
-      <div>
-        <Fieldset legend="Username" class="w-96">
-          <p>{{ username }}</p>
-        </Fieldset>
-      </div>
-      <div>
-        <Fieldset legend="Email" class="w-96">
-          <p>{{ email }}</p>
-        </Fieldset>
-      </div>
+  <div class="flex-col space-y-2.5 mt-6 w-96 place-items-center place-self-center space-y-8">
+    <div>
+      <Fieldset legend="Username" class="w-96">
+        <p>{{ username }}</p>
+      </Fieldset>
     </div>
-    <div class="mt-1">
-      <Button @click="logout">Log Out</Button>
+    <div>
+      <Fieldset legend="Email" class="w-96">
+        <p>{{ email }}</p>
+      </Fieldset>
     </div>
+  </div>
+  <div class="flex-col space-y-2.5 mt-8 w-96 place-self-center">
+    <Button @click="logout" class="w-96">Log Out</Button>
   </div>
 </template>
 
@@ -23,13 +21,18 @@ import { ref } from 'vue'
 import { useUserStore } from '../stores/user'
 import Fieldset from 'primevue/fieldset'
 import Button from 'primevue/button'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
 const userStore = useUserStore()
 const user = userStore.currentUser
 const username = ref('')
 const email = ref('')
 
-const logout = () => userStore.logout()
+const logout = () => {
+  userStore.logout()
+  router.push({ path: '/' })
+}
 
 function getData(user: { username: string; email: string }) {
   console.log(user)
