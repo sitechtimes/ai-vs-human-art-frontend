d930532bc1b881bc1d941d3db00a7989678b9891
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2025-01-03 11:02:33 -0500

    lolllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll

--- frontend/src/components/GamePage/GameArt.vue
+++ frontend/src/components/GamePage/GameArt.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="flex flex-col gap-2">
+  <div class="flex flex-col gap-2 h-[70vh]">
     <div v-if="artPieces.length" class="flex flex-col items-center max-w-screen">
       <div class="flex flex-row gap-2 w-full justify-between items-center max-h-20%]">
         <div class="flex flex-col max-w-[40vw] items-center">
-          <Image :src="artPieces[0]" alt="" class="flex m-7" preview aria-label="Image 1" />
+          <Image
+            :src="artPieces[0]"
+            alt=""
+            class="flex m-7 max-w-full h-[60vh]"
+            preview
+            aria-label="Image 1"
+          />
           <Button label="Image 1" class="flex self-center" @click="checkAnswer(0)"></Button>
         </div>
 
@@ -43,9 +49,15 @@ const isVisible = ref(false)
 const answer = ref(1) // which one is ai
 const correct = ref(false)
 
+const getFromBackend = async () => {
+  artPieces.value = [await artStore.getRandomArt('human'), await artStore.getRandomArt('ai')]
+  // the only reason this is pulled out is so
+}
+
 const getArt = async () => {
   isVisible.value = false
   artPieces.value = [await artStore.getRandomArt('human'), await artStore.getRandomArt('ai')]
+  // await getFromBackend()
   answer.value = 1
   if (artPieces.value.some((el) => el === null)) {
     alert('Failed to fetch art (boowomp)')
