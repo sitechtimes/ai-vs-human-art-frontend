8a4f647c547e7d197a1fc200de29aa6ca8b1a81b
Richard M. <richardm246@nycstudents.net>
2025-01-14 10:48:49 -0500

    no user checker

--- frontend/src/views/UserView.vue
+++ frontend/src/views/UserView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <!-- must add v-if for missing user -->
+  <!-- must add v-if for missing user -->
+  <div v-if="FoundUserData.value">
     <div
       class="flex flex-row bg-primary-50 dark:bg-primary-950 p-7 m-7 rounded-md border border-primary-700 dark:border-primary-500"
     >
@@ -14,6 +14,11 @@
       </div>
     </div>
   </div>
+  <div v-else>
+    <div class="flex flex-col justify-center p-5">
+      <h3 class="text-3xl">No User Found</h3>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -23,7 +28,7 @@ import { useRoute } from 'vue-router'
 const route = useRoute()
 import { useUserStore } from '../stores/user'
 const userStore = useUserStore()
-const FoundUserData = ref([])
+const FoundUserData = ref<any>([])
 try {
   const res = userStore
     .getUser(Number(route.params.id))
