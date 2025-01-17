7c7cb8969dfe39d38024152e9d19ada1c82467f4
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2025-01-17 12:31:49 -0500

    Rbbgghh

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -8,21 +8,13 @@
     <div class="bg-[url('/concept1animation.gif')] h-svh bg-cover bg-no-repeat static">
       <div class="absolute top-24 right-0 xl:right-32">
         <img src="/logo_black_white.gif" class="mt-16 sm:mt-0" />
-      </div>
-      <div class="pt-6">
-        <div class="grid lg:grid-cols-12 grid-cols-6 space-y-9">
-          <Button
-            as="router-link"
-            placeholder="Overriden"
-            class="!border-[2px] !border-primary-200 dark:!border-primary-700 !transition duration-300 ease-in-out delay-300 -rotate-6 dark:(hover:bg-primary-200) hover:!bg-primary-950 hover:scale-125 press-start-2p-regular lg:col-start-8 col-start-2 col-span-4 h-12 text-center !rounded-none mt-96"
-            label="gaym"
-            to="/game"
-          />
+        <div class="relative sm:left-32 flex flex-col w-11/12 mx-auto sm:w-80 sm:mx-0 gap-10">
+          <Button as="router-link" placeholder="Overriden" class="btn" label="gaym" to="/game" />
 
           <Button
             as="router-link"
             placeholder="Overriden"
-            class="!border-[3px] !border-primary-200 dark:!border-primary-700 !transition duration-300 ease-in-out delay-300 -rotate-6 hover:(dark:bg-primary-200) hover:!bg-primary-950 hover:scale-125 press-start-2p-regular lg:col-start-8 col-start-2 col-span-4 h-12 text-center !rounded-none mt-96"
+            class="btn"
             label="submit your art"
             to="/submit"
           />
@@ -30,7 +22,7 @@
           <Button
             as="router-link"
             placeholder="Overriden"
-            class="!border-[3px] !border-primary-200 dark:!border-primary-700 !transition duration-300 ease-in-out delay-300 -rotate-6 hover:!bg-primary-950 hover:scale-125 dark:(hover:!bg-primary-600) press-start-2p-regular lg:col-start-8 col-start-2 col-span-4 h-12 text-center !rounded-none mt-96"
+            class="btn"
             label="acknowledgments"
             to="/credits"
           />
@@ -50,5 +42,9 @@ import Button from 'primevue/button'
   font-family: 'Press Start 2P', system-ui;
   font-weight: 400;
   font-style: normal;
+}
+
+.btn {
+  @apply !border-[3px] border-primary-200 dark:border-primary-700 transition duration-300 ease-in-out -rotate-6 hover:scale-125 press-start-2p-regular h-12 text-center rounded-none hover:dark:!bg-red-500 hover:!bg-green-500;
 }
 </style>
