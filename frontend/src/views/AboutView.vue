84e6a4118d8156e1ed24b0b01022f3e5aa47f0b1
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2025-02-03 10:46:29 -0500

    bleh minor changes

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -3,15 +3,11 @@
     <div class="grid grid-cols-1 lg:grid-cols-2 justify-items-center">
       <PersonComponent v-for="person in persons" :key="person.name" :person="person" />
     </div>
-    <div>
-      <SubmitArtComponent />
-    </div>
   </div>
 </template>
 
 <script setup>
 import PersonComponent from '../components/PersonComponent.vue'
-import SubmitArtComponent from '@/components/SubmitArtComponent.vue'
 
 const persons = [
   { name: 'Katherine Z', blurb: null, image: null },
