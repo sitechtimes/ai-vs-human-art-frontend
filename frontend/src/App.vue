a782cadbe27ec75a5813a6a65775d6faffe12c84
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2025-02-04 11:02:27 -0500

    vc xbn mhgfdxsf

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,15 +5,12 @@ import ThemeComponent from './components/ThemeComponent.vue'
 </script>
 
 <template>
-  <div>
-    <HeaderComponent/>
+  <div class="font-display">
+    <HeaderComponent />
     <!--should probaby put this on the home page; in app for now can we move this later when katherine is done or smth-->
     <ThemeComponent />
     <RouterView />
   </div>
 </template>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
