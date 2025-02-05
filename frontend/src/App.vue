c1a8b0fc9a25965351624e496ec7f7546e890a56
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2025-02-05 10:50:11 -0500

    blreh bleh blegrdefhgsfdgfasdg yay we diecided

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@ import ThemeComponent from './components/ThemeComponent.vue'
 </script>
 
 <template>
-  <div class="font-display">
+  <div>
     <HeaderComponent />
     <!--should probaby put this on the home page; in app for now can we move this later when katherine is done or smth-->
     <ThemeComponent />
