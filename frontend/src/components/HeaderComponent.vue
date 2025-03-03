4ee2ecbe61537c6bad15f1357aa9ccd36dbd4aec
Richard M. <richardm246@nycstudents.net>
2025-03-03 10:56:23 -0500

    Profilepic hotfix

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -11,17 +11,17 @@
           </a>
         </router-link>
         <!--         <a v-else :href="item.url" :target="item.target" v-bind="props.action">
-          <span class="ml-2">{{ item.label }}</span>
-           put something here to indicate that this is a dropdown i don't know
-        </a> -->
+            <span class="ml-2">{{ item.label }}</span>
+            put something here to indicate that this is a dropdown i don't know
+          </a> -->
       </template>
       <template #end>
         <div class="flex items-center gap-0.5" v-if="userStore.currentUser">
           <router-link to="/profile">
             <img
-              :src="userStore.profile_picture || '/userImg.svg'"
+              :src="userStore.currentUser.profile_picture || '/userImg.svg'"
               alt="placeholder avatar"
-              class="rounded-full dark:invert"
+              class="rounded-full"
             />
           </router-link>
         </div>
