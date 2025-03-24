0eed3b089f2d69f535fa559a61c69d12eb7cb0a3
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2025-03-24 11:03:27 -0400

    FCUKKK MY LIFE I CANT PASS IAMGES THRU AN ARRAY???

--- frontend/src/components/SubmitArtComponent.vue
+++ frontend/src/components/SubmitArtComponent.vue
@@ -34,7 +34,7 @@
             accept="image/*"
             :maxFileSize="1024 * 1024 * 15"
             customUpload
-            @select="uploadedFile"
+            @select="uploadedFile($event, index)"
             label="Upload an image"
           />
         </div>
@@ -84,6 +84,7 @@ const pictures = ref([
     file: ''
   }
 ])
+const allFormData = ref([])
 
 const addNewPicture = () => {
   pictures.value.push({
@@ -106,8 +107,8 @@ const addToast = (severity, summary, detail) => {
   })
 }
 
-const uploadedFile = (e) => {
-  picture.file = e.files[0]
+const uploadedFile = (e, index) => {
+  pictures.value[index].file = e.files[0]
   addToast('success', 'Success', 'File sucessfully uploaded.')
 }
 
@@ -126,16 +127,15 @@ const submit = async () => {
 
   const formData = new FormData()
   pictures.value.forEach((picture) => {
-    formData.append(`link`, picture.link)
+    formData.append(`tag`, picture.link)
     formData.append('name', picture.name)
     formData.append('image', picture.file)
-    picture.link = ''
-    picture.name = ''
-    picture.file = null
+    console.log(picture.name, picture.file)
   })
   formData.append('type', 'unscreened')
+  allFormData.value.push(formData)
 
-  const res = await imageStore.uploadImage(formData)
+  const res = await imageStore.uploadImage(allFormData)
   if (!res.ok) {
     addToast('error', 'Error', 'Failed to submit art.')
     throw new Error((await res.json()).error)
@@ -145,9 +145,10 @@ const submit = async () => {
     {
       link: '',
       name: '',
-      file: ''
+      file: null
     }
   ]
+  allFormData.value = []
   uploading.value = false
 }
 </script>
--- frontend/src/stores/images.js
+++ frontend/src/stores/images.js
@@ -2,11 +2,14 @@ import { defineStore } from 'pinia'
 const backendUrl = import.meta.env.VITE_PUBLIC_BACKEND
 
 export const useImageStore = defineStore('image', () => {
-  const uploadImage = async (formData) => {
+  const uploadImage = async (data) => {
     const requestOptions = {
       method: 'POST',
-      headers: { Authorization: `Bearer ${localStorage.token}` },
-      body: formData
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
     }
 
     const res = await fetch(`${backendUrl}/items/upload`, requestOptions)
