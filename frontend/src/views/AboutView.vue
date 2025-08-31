6dcbd93207df30271dec51167e2757e03fd6c40a
katherine <katherineze26@gmail.com>
2025-08-31 17:04:14 -0400

    sadsadsadsadsadasdasd

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="mt-[58px]">
-    <div class="grid grid-cols-1 lg:grid-cols-2 justify-items-center">
-      <div
+  <div class="mt-16">
+    <h1 class="font-bold text-xl md:text-2xl text-center underline">THE TEAM</h1>
+    <p class="text-center text-sm">Behold, the rag-tag team of teenagers. </p>
+    <div class="grid grid-rows-4 lg:grid-cols-4 justifdy-items-center">
+      <div 
         v-for="person in persons"
         :key="person.name"
         :person="person"
-        class="flex flex-row bg-primary-50 dark:bg-primary-950 p-7 m-7 rounded-md border border-primary-700 dark:border-primary-500 w-5/6"
+        class="flex flex-col bg-primary-50 dark:bg-primary-950 p-7 m-7 rounded-md border border-primary-700 dark:border-primary-500 w-5/6"
       >
         <img
-          class="object-contain w-52 h-80 flex self-center"
+          class="object-contain w-52 h-60 flex place-self-center"
           :src="person.image"
           :alt="person.name"
         />
+      
         <div class="flex flex-col justify-center p-5">
           <h3 class="text-3xl">{{ person.name }}</h3>
           <p class="text-sm">{{ person.blurb }}</p>
@@ -23,10 +26,10 @@
 
 <script setup>
 const persons = [
-  { name: 'Katherine Z', blurb: null, image: null },
-  { name: 'Tina G', blurb: 'Staten Island Technical High School graduate and self  taught artist!! I love making keychains and other types of merch for my favorite characters <3. AND the coolest person you will ever meet.', image: '../../public/tinaabout.png'  },
-  { name: 'Richard M', blurb: null, image: null },
-  { name: 'Yifan Z', blurb: 'Graduated from Staten Island Technical High School in 2025. A big fan of fish and virtual fishing. You can probably find me reading manga.', image: '../../public/worm.png' }
+  { name: 'Katherine Z', blurb: "Hi! Currently a high school senior, and somehow always tired. Outside of art, I try to dabble in music, game dev, writing, and their relation to math, logic, and science: how the world turns. Talk to me about Limbus Company, Arknights, Bocchi the Rock, Genshin Sumeru, Vocaloid, and Umamusume!", image: '../../ppfpfp.png' },
+  { name: 'Tina G', blurb: 'Staten Island Technical High School graduate and self  taught artist!! I love making keychains and other types of merch for my favorite characters <3. AND the coolest person you will ever meet.', image: '../../tinaabout.png'  },
+  { name: 'Richard M', blurb: "to be added LOL", image: null },
+  { name: 'Yifan Z', blurb: 'Graduated from Staten Island Technical High School in 2025. A big fan of fish and virtual fishing. You can probably find me reading manga.', image: '../../worm.png' }
 ]
 </script>
 
